d3ae63eab87abe1aa28359fe1ddcf5d267aa4de7
Mohammad Samian Yusuf <s.yusuf91@hotmail.com>
2018-12-21 12:00:30 -0500

    Add files via upload
    image blurring seems to be working

--- oven_light.py
+++ oven_light.py
@@ -1,24 +1,31 @@
 import os
 import numpy as np
 import cv2
+import tkinter
 
 
 def main():
-    img = cv2.imread('oven_on.jpg',1)
-    cv2.imshow('image',img)
-    final_pic=cv2.medianBlur(img,9)
-    cv2.imshow('image',final_pic)
-    
+    img = cv2.imread(
+        "C:\\Users\\attackpiggy\\Desktop\\oven_projects\\oven\\rsz_oven_on.jpg")
+    # img=cv2.imread(
+    #     "C:\\Users\\attackpiggy\\Desktop\\oven_projects\\oven\\oven_on.jpg")
+
+    # cv2.namedWindow('img', cv2.WINDOW_NORMAL)
+    # imS = cv2.resize(img, (960, 540))
+    # blurring image
+    median = cv2.medianBlur(img, 9)
+
+    cv2.imshow('IMAGE', median)
+
     k = cv2.waitKey(0)
     if k == 27:         # wait for ESC key to exit
         cv2.destroyAllWindows()
-    elif k == ord('s'): # wait for 's' key to save and exit
-        cv2.imwrite('oven_onigray.png',img)
+    elif k == ord('s'):  # wait for 's' key to save and exit
+
+        cv2.imwrite('oven_onigray.png', img)
         cv2.destroyAllWindows()
 
-     #now to blur the image 
-       
-
+        # now to blur the image
 
 
 if __name__ == "__main__":
